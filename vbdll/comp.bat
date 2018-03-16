86067ef4b5cde20cdd2298c8711782506917ccd6
Cassio Butrico <cassio_butrico@hotmail.com>
2018-03-16 16:38:29 -0300

    no message

---
+++ vbdll/comp.bat
@@ -0,0 +1,22 @@
+@echo off
+rem **********************************************
+rem dmd -O -shared  -offuncoes.dll funcoes.d dll.d 
+rem dmd -O -lib -offuncoes.lib funcoes.d dll.d 
+rem ***********************************************
+dmd -O  -offuncoes.dll -L/IMPLIB  funcoes.d dll.d defuncoes.def
+dll2def funcoes.dll
+path=%vb%;%path%
+vb6 Clasifica.vbp /Make
+del *.obj
+call test.bat
+exit
+
+
+ 
+
+
+
+
+
+
+
