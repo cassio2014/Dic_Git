4bc8e1d14104db05eda4793bc5e17668cedde65b
Cassio2014 <cassio_butrico@hotmail.com>
2018-07-19 19:32:56 -0300

    no message

--- auxiliar/exemplos em D/Dll em D/Cria lib do Dll convert compila D/comp.bat
+++ auxiliar/exemplos em D/Dll em D/Cria lib do Dll convert compila D/comp.bat
@@ -1,11 +1,14 @@
 @echo off
+criadef  imprimir.dll
 rc resource.rc
-"C:\Program Files\Microsoft Visual Studio\VC98\bin\link" /dump /exports imprimir.dll /out:imprimir.txt
-dll2def  imprimir.dll > imprimir.def
-"C:\Program Files\Microsoft Visual Studio\VC98\bin\lib" /def:imprimir.def   /OUT:imp.lib /MACHINE:x86
+path=%vc%;path
+link /dump /exports imprimir.dll /out:imprimir.txt
+lib /def:imprimir.def   /OUT:imp.lib /MACHINE:x86
+path=%dc%;path
 coffimplib imp.lib  imprimir.lib
 dmd -O -release lerdll.d resource.RES imprimir.lib
-del imp.lib *.exp *.obj
+#pause
+del *.exp *.obj
 @cls
 @lerdll
 
