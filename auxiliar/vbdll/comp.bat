b9f563fa46ab10fd3e80cf993276c91e85e94e78
Cassio2014 <cassio_butrico@hotmail.com>
2018-07-31 20:29:32 -0300

    no message

--- auxiliar/vbdll/comp.bat
+++ auxiliar/vbdll/comp.bat
@@ -3,8 +3,9 @@ rem **********************************************
 rem dmd -O -shared  -offuncoes.dll funcoes.d dll.d 
 rem dmd -O -lib -offuncoes.lib funcoes.d dll.d 
 rem ***********************************************
-dmd -O  -offuncoes.dll -L/IMPLIB  funcoes.d dll.d defuncoes.def
-criadef funcoes.dll
+dmd -O  -offuncoes.dll -L/IMPLIB  funcoes.d dll.d 
+criadef  funcoes.dll
+pause
 path=%vb%;%path%
 vb6 Clasifica.vbp /Make
 del *.obj
