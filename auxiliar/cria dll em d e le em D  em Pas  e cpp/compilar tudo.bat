05a41aac61490227896e87d1d20bcfbc09a15236
Cassio Butrico <cassio_butrico@hotmail.com>
2022-04-27 18:51:40 -0300

    up

--- auxiliar/cria dll em d e le em D  em Pas  e cpp/compilar tudo.bat
+++ auxiliar/cria dll em d e le em D  em Pas  e cpp/compilar tudo.bat
@@ -5,9 +5,9 @@ copy imprimir.dll ".\lendo a dll en D"
 copy imprimir.lib ".\lendo a dll en D"
 copy resd.res ".\lendo a dll en D"
 dmd DlerDll.d  resd.res
-path=%vb%
+path=%vb%;%path%
 link /dump /exports imprimir.dll /out:imprimir.txt
-path=%bcc32%
+path="C:\Borland\BCC55\Bin"
 bcc32 -w-  lerimprimir
 del *.obj
 del *.tds
